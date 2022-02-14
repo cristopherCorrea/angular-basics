b126a2acbc8fde7f16154247b0d5dbc779110821
Cristopher Correa Leyva <dos.osos.apestosos@gmail.com>
2022-02-13 18:26:04 -0800

    adding component using template intead templateUrl

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  //you can use template instead of templateUrl to write the html, use it only if the html is shot ``for many lines ''only one line html
+  template: `<p>servers works with template!</p>
+  <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
