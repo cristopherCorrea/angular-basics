c4ad001081f560869c0f21b66a669b7168abc248
Cristopher Correa Leyva <dos.osos.apestosos@gmail.com>
2022-02-13 18:51:27 -0800

    adding two new component for practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { successAlertComponent } from './successAlert/successAlert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    successAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    styleUrls: ['./successAlert.component.css'],
+    templateUrl: './successAlert.component.html',
+    selector: 'app-sucess-alert'
+
+})
+export class successAlertComponent {
+
+}
